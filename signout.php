57e301da0b1d38972ce8745e2039c7ab24d972ff
Kibetinho <drewkibet@gmail.com>
2025-08-05 12:41:14 +0300

    signout.php

---
+++ signout.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/../includes/auth_check.php';
+require_once __DIR__ . '/../includes/db_connect.php';
+include __DIR__ . '/../includes/header.php';
+
+// ...existing code...
