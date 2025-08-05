5821894d8d29f0a4e42c81aa49e6f78545c1613d
Kibetinho <drewkibet@gmail.com>
2025-08-05 12:37:09 +0300

    admin.php

---
+++ admin.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header("Location: auth/login.php");
+    exit;
+}
+
+require_once __DIR__ . '/../includes/auth_check.php';
+require_once __DIR__ . '/../includes/db_connect.php';
+include __DIR__ . '/../includes/header.php';
+?>
+<link rel="stylesheet" href="assets/styles.css">
+<script src="assets/script.js" type="module"></script>
